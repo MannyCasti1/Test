683b14594d90c52d34d2cea1efc64fba0f10d1d8
UHManuel <70231911+UHManuel@users.noreply.github.com>
2021-02-27 18:37:22 -0600

    Delete L.py

--- L.py
+++
@@ -1,6 +0,0 @@
-class User:
-  def __init__(self, id, firstname, lastname, dateofbirth):
-    self.id = id
-    self.firstname = firstname
-    self.lastname = lastname
-    self.dateofbirth = dateofbirth
